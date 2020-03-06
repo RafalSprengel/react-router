3f7b24d8263b369ecdd215b69d87f8698ec555c7
Rafal66 <sprengel.rafal@gmail.com>
2020-03-06 11:51:08 +0000

    Updates

---
+++ precache-manifest.3de57ffd39c0d496c62a3697b5e92ad0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8c1b6953087d8c5e0047c6a0c0e74f",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "d52a1c7d5294eafa143f",
+    "url": "/react-router/static/css/main.99e71c29.chunk.css"
+  },
+  {
+    "revision": "3aeed014645d7178e1ed",
+    "url": "/react-router/static/js/2.a8abc375.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-router/static/js/2.a8abc375.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d52a1c7d5294eafa143f",
+    "url": "/react-router/static/js/main.1e1d1d27.chunk.js"
+  },
+  {
+    "revision": "c2ec8549b1466c5cdb86",
+    "url": "/react-router/static/js/runtime-main.c18cedf5.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "8f90564699a2a266cf4134b0c4df171d",
+    "url": "/react-router/static/media/site-not-found.8f905646.png"
+  }
+]);
