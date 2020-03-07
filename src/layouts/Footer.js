10fefd89b148c35abdbcd1b60628af0c77168f1b
Rafal66 <sprengel.rafal@gmail.com>
2020-03-07 21:48:32 +0000

    changes

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,11 +2,15 @@ import React from 'react';
 import '../css/Footer.css'
 import { Route } from 'react-router-dom';
 
-const Foot = ({ routeProps }) => (
-    <span style={{ color: 'white' }}>
-        you are in: <strong>{routeProps.match.params.page}</strong>
-    </span>
-)
+const Foot = ({ routeProps }) => {
+    console.log('props:');
+    console.log(routeProps)
+    return (
+        <span style={{ color: 'white' }}>
+            you are in: <strong>{routeProps.match.params.page}</strong>
+        </span>
+    )
+}
 
 const Footer = () => {
     return (
